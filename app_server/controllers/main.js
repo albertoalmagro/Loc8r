d1c387b667517ebf71603c7c10bff4f15e19db96
Alberto Almagro <albertoalmagro@gmail.com>
2014-10-28 20:54:27 +0100

    Moved data from 'generic-text' template to controller

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,9 @@
 /* GET 'about us' page */
 exports.about = function(req, res){
-    res.render('generic-text', { title: 'About us' });
+    res.render('generic-text', {
+        title: 'About Loc8r',
+        content: 'Loc8r was created to help people find places to sit down and get a little bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan.'
+    });
 };
 
 /* GET 'sign in' page */
