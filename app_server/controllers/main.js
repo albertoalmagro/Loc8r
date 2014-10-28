6a383ed34544089153e2bf9e7620f785a0a04622
Alberto Almagro <albertoalmagro@gmail.com>
2014-10-28 17:21:01 +0100

    Added generic-text template and used it for the about us page

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET 'about us' page */
 exports.about = function(req, res){
-    res.render('index', { title: 'About us' });
+    res.render('generic-text', { title: 'About us' });
 };
 
 /* GET 'sign in' page */
